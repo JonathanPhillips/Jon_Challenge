8dba8c5db6e6b107c9fc9f3a2f9f050351f96a1e
JonathanPhillips <jon7035@gmail.com>
2022-06-27 11:04:15 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,14 +138,6 @@ resource "aws_security_group" "nginx-sg" {
   name   = "nginx_sg"
   vpc_id = aws_vpc.vpc.id
 
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # HTTP access from the VPC
   ingress {
     from_port   = 80
